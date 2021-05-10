24bcba4e2faba6c4912b073053a15ca20621d2a0
Roma <roma01@yandex.ru>
2021-05-10 13:18:59 +0300

    first commit

--- temp.py
+++ temp.py
@@ -63,6 +63,9 @@ external_stylesheets = [
     },
 ]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
 app.title = "Panda Analytics: Choose Your Panda!"
 
 app.layout = html.Div(
