458a6907fcd9fdf465f2a1f3626308b48bf96d81
Roma <roma01@yandex.ru>
2021-05-13 02:47:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -179,6 +179,15 @@ app.layout = html.Div(
             className="wrapper",
         ),
     ]
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J6LXXTXN59"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-J6LXXTXN59');
+</script>
 )
 
 
